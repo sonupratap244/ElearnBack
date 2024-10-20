b87d84ba53b6c8e796f2db9a3aa5d6a450952be3
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2024-10-20 11:24:59 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,10 +18,10 @@ const Home = () => {
       <div className="home">
         <div className="home-content">
           <h1 data-aos="fade-down"
-     data-aos-easing="linear" data-aos-duration="1500">Welcome to Our Vinayak Study-Zone.</h1>
+     data-aos-easing="linear" data-aos-duration="1500">Welcome to Our Vinayak Study Zone.</h1>
           <h2 data-aos="fade-up"
      data-aos-duration="3000">Unlock Your Career Potential by <br/>Learning Any Skill</h2>
-          <p>Start Your Online Tutoring or Consultation Marketplace with Study-Zone.</p>
+          <p>Start Your Online Tutoring or Consultation Marketplace with Study Zone.</p>
           <button onClick={()=>navigate("/courses")} className="common-btn" data-aos="flip-right">Get Started</button>
         </div>
       </div>
